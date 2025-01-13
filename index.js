7156b1b7f1063e66e2e1e97c119241e56dd00c22
kd pc <dongkp96@gmail.com>
2025-01-13 14:38:24 -0500

    Sets up project with initial HTML and JS

---
+++ index.js
@@ -0,0 +1,26 @@
+const library = [];
+
+function book(title, author, pages, readStatus){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+book.prototype.addToLibrary = function(library){
+    library.push(this);
+}
+/*Adds a function called addToLibrary to the book.prototype
+uses the push method to add to the Array called library and uses the "this" keyword
+to add the book object to the library */
+
+
+const hobbit = new book("The Hobbit", "J.R.R Tolkien", 295, true);
+
+
+hobbit.addToLibrary(library);
+
+for(let i =0; i <library.length; i++){
+    alert(library[i]);
+}
+
